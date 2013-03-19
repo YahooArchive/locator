c74da12de49906badb194f65f1fb8fab935733a9
Drew Folta <drew@folta.net>
2013-03-19 14:34:27 -0700

    docs

--- tests/lib/index.js
+++ tests/lib/index.js
@@ -1,4 +1,10 @@
-// TODO COPYRIGHT
+/*
+ * Copyright (c) 2013, Yahoo! Inc.  All rights reserved.
+ * Copyrights licensed under the New BSD License.
+ * See the accompanying LICENSE.txt file for terms.
+ */
+
+
 /*jslint nomen:true, anon:true, node:true */
 /*globals describe,it */
 "use strict";
