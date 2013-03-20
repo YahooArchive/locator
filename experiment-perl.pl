d339baa5445c3ecf7d7577fbaeb40ee9dd25c12f
Drew Folta <drew@folta.net>
2013-03-19 17:22:54 -0700

    experiment continues

--- experiment-perl.pl
+++ experiment-perl.pl
@@ -4,8 +4,21 @@ use strict;
 use warnings;
 use Data::Dumper;
 
+our @FIND = qw(
+    Yahoo::Backyard::SingleSignOn
+    HTTP::Status
+    XML::LibXML
+);
+
 sub main {
-    print Dumper(\@INC);
+    my @found;
+    foreach my $find ( @FIND ) {
+        eval "use $find";
+        unless ($@) {
+            push @found, $find;
+        }
+    }
+    print Dumper(\@found);
 }
 main();
 
