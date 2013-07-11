0a3cc8375c0a33901022d0fbb57f72ee6e50072e
Isao Yagi <isao@yahoo-inc.com>
2013-07-11 09:53:29 -0700

    new: custom bundle build dir basenames w/ parseBundle() option
    Allow bundle build directory naming to be specified by a function
    passed as a parameter to parseBundle via option object key named
    "buildDirectoryNamer". The function is passed the bundle object
    (see lib/bundle.js).
    
    i.e.
    
        var loc = new Locator({buildDirectory: 'build'}),
            opts = {buildDirectoryNamer: function(bundle) {
                    return bundle.name + '_' + bundle.version;
                }};
    
        loc.parseBundle(__dirname, opts).then(/* ... */);

---
+++ tests/lib/buildDirectory.js
@@ -0,0 +1,34 @@
+var join = require('path').join,
+    expect = require('chai').expect,
+    BundleLocator = require('../../lib/bundleLocator.js'),
+    fixturesPath = join(__dirname, '../fixtures');
+
+
+describe('touchdown-simple', function () {
+    var fixtureDir = join(fixturesPath, 'touchdown-simple'),
+        locator = new BundleLocator({
+            applicationDirectory: fixtureDir,
+            buildDirectory: 'build'
+        }),
+        options = {
+            // for build dir name "roster__0-0-1" instead of "roster-0.0.1"
+            buildDirectoryNamer: function(bundle) {
+                return bundle.name + '__' + bundle.version.replace(/\./g, '-');
+            }
+        },
+        rootHave,
+        //rootWant = require(join(fixtureDir, 'expected-locator.js')),
+        expected = join(fixtureDir + '/build/roster__0-0-1');
+
+
+    before(function (next) {
+        locator.parseBundle(fixtureDir, options).then(function (bundle) {
+            rootHave = bundle;
+            next();
+        }, next);
+    });
+
+    it('parseBundle()', function () {
+        expect(rootHave.bundles.roster.buildDirectory).to.equal(expected);
+    });
+});
