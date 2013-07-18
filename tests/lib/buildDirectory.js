ad8a887b279ea55154bc3defe1b4b19b68dbcec8
Isao Yagi <isao@yahoo-inc.com>
2013-07-18 12:30:12 -0700

    set this._options.bundleBuildDirectoryParser conditionally
    This option property may be applied in the constructor and/or
    parseBundle().

--- tests/lib/buildDirectory.js
+++ tests/lib/buildDirectory.js
@@ -4,31 +4,78 @@ var join = require('path').join,
     fixturesPath = join(__dirname, '../fixtures');
 
 
-describe('touchdown-simple', function () {
+describe('default bundle build dir', function () {
+    var fixtureDir = join(fixturesPath, 'touchdown-simple'),
+        locator = new BundleLocator({
+            applicationDirectory: fixtureDir,
+            buildDirectory: 'build'
+        }),
+        expected = join(fixtureDir + '/build/roster-0.0.1'),
+        actual;
+
+    before(function (next) {
+        locator.parseBundle(fixtureDir /* NO OPTIONS */).then(function (bundle) {
+            actual = bundle;
+            next();
+        }, next);
+    });
+
+    it('parseBundle()', function () {
+        expect(actual.bundles.roster.buildDirectory).to.equal(expected);
+    });
+});
+
+describe('custom bundle build dir via `parseBundle` option', function () {
     var fixtureDir = join(fixturesPath, 'touchdown-simple'),
         locator = new BundleLocator({
             applicationDirectory: fixtureDir,
             buildDirectory: 'build'
         }),
         options = {
-            // for build dir name "roster__0-0-1" instead of "roster-0.0.1"
             bundleBuildDirectoryParser: function(bundle) {
+                // for build dir name "roster__0-0-1" instead of "roster-0.0.1"
                 return bundle.name + '__' + bundle.version.replace(/\./g, '-');
             }
         },
-        rootHave,
-        //rootWant = require(join(fixtureDir, 'expected-locator.js')),
+        actual,
         expected = join(fixtureDir + '/build/roster__0-0-1');
 
 
     before(function (next) {
         locator.parseBundle(fixtureDir, options).then(function (bundle) {
-            rootHave = bundle;
+            actual = bundle;
             next();
         }, next);
     });
 
     it('parseBundle()', function () {
-        expect(rootHave.bundles.roster.buildDirectory).to.equal(expected);
+        expect(actual.bundles.roster.buildDirectory).to.equal(expected);
     });
 });
+
+describe('custom bundle build dir via `new Locator` option', function () {
+    var fixtureDir = join(fixturesPath, 'touchdown-simple'),
+        locator = new BundleLocator({
+            applicationDirectory: fixtureDir,
+            buildDirectory: 'build',
+            bundleBuildDirectoryParser: function(bundle) {
+                // for build dir name "roster__0-0-1" instead of "roster-0.0.1"
+                return bundle.name + '__' + bundle.version.replace(/\./g, '-');
+            }
+        }),
+        actual,
+        expected = join(fixtureDir + '/build/roster__0-0-1');
+
+
+    before(function (next) {
+        locator.parseBundle(fixtureDir /* NO OPTIONS */).then(function (bundle) {
+            actual = bundle;
+            next();
+        }, next);
+    });
+
+    it('parseBundle()', function () {
+        expect(actual.bundles.roster.buildDirectory).to.equal(expected);
+    });
+});
+
